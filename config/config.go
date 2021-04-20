0dcfdeaea1600a3542159f80c524bf466cd33a09
Ano Van <anovanmaximuz@gmail.com>
2021-04-20 22:41:58 +0700

    Update config.go

--- config/config.go
+++ config/config.go
@@ -8,7 +8,7 @@ import (
 // DBInit create connection to database
 func DBInit() *gorm.DB {
 	//db, err := gorm.Open("mysql", "root:@tcp(128.199.211.144:3306)/godb?charset=utf8&parseTime=True&loc=Local")
-	db, err := gorm.Open("mysql","root:orion2402@(localhsot:3306)/popfren?charset=utf8&parseTime=True&loc=Local")
+	db, err := gorm.Open("mysql","root:orion2402@tcp(localhsot:3306)/popfren?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic("failed to connect to database")
 	}
